11d2dfac3b582224d6e2b68314494d2b34d23149
Changhow Li <wy2857749@163.com>
2026-08-31 17:37:05 +0800

    add boot.py and main.py

---
+++ boot.py
@@ -0,0 +1,2 @@
+if __name__ == "__main__":
+    print("This is boot.py. And is nothing to do, exit")
---
+++ main.py
@@ -0,0 +1,2 @@
+if __name__ == "__main__":
+    print("This is main.py. And is nothing to do, enter REPL")
